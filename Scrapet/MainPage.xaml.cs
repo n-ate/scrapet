9ae629643d566a7dc40a5784cd8fb114d8182eb9
Nate Layton <NXL01@acuitysso.com>
2022-12-01 14:09:06 -0500

    Basic Maui Blazor project.

---
+++ Scrapet/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Scrapet.Resources;
+
+namespace Scrapet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            if (sender is WebView view)
+            {
+                var value = await view.EvaluateJavaScriptAsync(Scripts.Selection);
+            }
+        }
+    }
+}
